0261653c1e298b24433537c9cc22821695d29f41
AllenThomasDev <allen.thomas.dev@gmail.com>
2020-07-06 16:04:28 +0530

    all player attributes now included in a single dictionary

--- players.py
+++ players.py
@@ -3,28 +3,18 @@ import random
 import numpy as np
 class Player:
     def __init__(self,row):
-        self.shooting=row['shooting']
-        self.defending=row['defending']
-        self.passing=row['passing']
-        self.pace=row['pace']
-        self.physical=row['physic']
-        self.dribbling=row['dribbling']
-        self.name=row['long_name']
-        self.overall=row['overall']
-        self.nationality=row['nationality']
-        self.value=row['value_eur']
-        self.club=row['club']
-        self.position=row['player_positions']
+        self.stats=row.to_dict()
+        self.stats['team_position']='Reserves'
         self.setPosition()
-        self.team_position='Reserves'
+        
 
     def setPosition(self):
-        if 'B' in self.position.split(",")[0]:
-            self.position='Defender'
-        if 'M' in self.position.split(",")[0]:
-            self.position='Midfielder'
+        if 'B' in self.stats['player_positions'].split(",")[0]:
+            self.stats['player_positions']='Defender'
+        if 'M' in self.stats['player_positions'].split(",")[0]:
+            self.stats['player_positions']='Midfielder'
         for x in ['S','F','W']:
-            if x in self.position.split(",")[0]:
-                self.position='Attacker'
+            if x in self.stats['player_positions'].split(",")[0]:
+                self.stats['player_positions']='Attacker'
         
         pass
--- teams.py
+++ teams.py
@@ -9,7 +9,7 @@ from playerlist import df
 from tabulate import tabulate
 
 class Team:
-    attlist=['name','nationality', 'overall', 'position','team_position', 'pace','shooting', 'passing', 'dribbling', 'defending', 'physical']
+    attlist=['long_name','nationality', 'overall', 'player_positions','team_position', 'pace','shooting', 'passing', 'dribbling', 'defending', 'physical']
     def __init__(self,team_name):
         self.players={}
         self.squad={}
@@ -25,9 +25,9 @@ class Team:
         self.manager=None
         self.setManager()
         self.playingSquad()
-        self.attack=round(np.array([a.overall for a in self.players.values() if a.position=='Attacker' and (a.team_position=='Starter' or a.team_position=='Substitute')]).mean())
-        self.midfield=round(np.array([a.overall for a in self.players.values() if a.position=='Midfielder' and (a.team_position=='Starter' or a.team_position=='Substitute')]).mean())
-        self.defence=round(np.array([a.overall for a in self.players.values() if (a.position=='Defender' or a.position=='GK') and (a.team_position=='Starter' or a.team_position=='Substitute')]).mean())
+        self.attack=round(np.array([a.stats['overall'] for a in self.players.values() if a.stats['player_positions']=='Attacker' and (a.stats['team_position']=='Starter' or a.stats['team_position']=='Substitute')]).mean())
+        self.midfield=round(np.array([a.stats['overall'] for a in self.players.values() if a.stats['player_positions']=='Midfielder' and (a.stats['team_position']=='Starter' or a.stats['team_position']=='Substitute')]).mean())
+        self.defence=round(np.array([a.stats['overall'] for a in self.players.values() if (a.stats['player_positions']=='Defender' or a.stats['player_positions']=='GK') and (a.stats['team_position']=='Starter' or a.stats['team_position']=='Substitute')]).mean())
         # self.playingSquad()
 
     def addPlayers(self):
@@ -56,18 +56,18 @@ class Team:
     
     def populatePositions(self):
         for player in self.players.values():
-            if player.position =='Attacker':
-                self.attackers[player.name]=player
-            if player.position =='Defender':
-                self.defenders[player.name]=player
-            if player.position =='Midfielder':
-                self.midfielders[player.name]=player
-            if player.position =='GK':
-                self.goalkeepers[player.name]=player
+            if player.stats['player_positions'] =='Attacker':
+                self.attackers[player.stats['long_name']]=player
+            if player.stats['player_positions'] =='Defender':
+                self.defenders[player.stats['long_name']]=player
+            if player.stats['player_positions'] =='Midfielder':
+                self.midfielders[player.stats['long_name']]=player
+            if player.stats['player_positions'] =='GK':
+                self.goalkeepers[player.stats['long_name']]=player
 
     def showManager(self):
         table_data=[]
-        table_data.append([self.manager.name,'Manager',self.manager.stats,self.manager.ivs,self.manager.formation])
+        table_data.append([self.manager.stats['long_name'],'Manager',self.manager.stats,self.manager.ivs,self.manager.formation])
         print(tabulate(table_data))
 
     def showTeam(self,player_dict=None,position=['striker','midfielder','defender','goalkeeper']):
@@ -80,36 +80,37 @@ class Team:
 
     def showStats(self):
         print(f"{self.team_name}'s Stats\nAttack = {self.attack}\nMidfield = {self.midfield}\nDefence = {self.defence}")
+        
     def playingSquad(self):
         squad={}
         attackers=list(self.attackers.values())
-        attackers.sort(key = lambda x:x.overall,reverse=True)
+        attackers.sort(key = lambda x:x.stats['overall'],reverse=True)
         for player in attackers[0:self.manager.strikers]:
-            player.team_position='Starter'
-            squad[player.name]=player
+            player.stats['team_position']='Starter'
+            squad[player.stats['long_name']]=player
 
         midfielders=list(self.midfielders.values())
-        midfielders.sort(key = lambda x:x.overall,reverse=True)
+        midfielders.sort(key = lambda x:x.stats['overall'],reverse=True)
         for player in midfielders[0:self.manager.midfielders]:
-            player.team_position='Starter'
-            squad[player.name]=player
+            player.stats['team_position']='Starter'
+            squad[player.stats['long_name']]=player
 
         defenders=list(self.defenders.values())
-        defenders.sort(key = lambda x:x.overall,reverse=True)
+        defenders.sort(key = lambda x:x.stats['overall'],reverse=True)
         for player in defenders[0:self.manager.defenders]:
-            player.team_position='Starter'
-            squad[player.name]=player
+            player.stats['team_position']='Starter'
+            squad[player.stats['long_name']]=player
 
         goalkeepers=list(self.goalkeepers.values())
-        goalkeepers.sort(key = lambda x:x.overall,reverse=True)
-        goalkeepers[0].team_position='Starter'
-        squad[goalkeepers[0].name]=goalkeepers[0]
+        goalkeepers.sort(key = lambda x:x.stats['overall'],reverse=True)
+        goalkeepers[0].stats['team_position']='Starter'
+        squad[goalkeepers[0].stats['long_name']]=goalkeepers[0]
         self.squad=squad
 
     def Substitutes(self):
         subs=[x for x in self.players.keys() if x not in self.squad.keys()][:7]
         for player in subs:
-            self.players[player].team_position='Substitute'
+            self.players[player].stats['team_position']='Substitute'
             self.subs[player]=self.players[player]
         reserves = [r for r in self.players.items() if (r not in self.squad.items() and r not in self.subs.items())]
         self.reserves.update(reserves)
