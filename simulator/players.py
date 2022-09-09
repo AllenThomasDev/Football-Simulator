160b8648d8dee34562e495503fc3d31c05bff317
Megh <meghkhaire@gmail.com>
2022-09-10 00:01:25 +0530

    Move manager names to constants module

--- simulator/players.py
+++ simulator/players.py
@@ -1,28 +1,30 @@
-from names import *
-import random
-import numpy as np
+import pandas as pd
 from tabulate import tabulate
+
+
 class Player:
-    facestats=['short_name','nationality', 'overall', 'player_positions','team_position', 'pace','shooting', 'passing', 'dribbling', 'defending', 'physic']
-    def __init__(self,row):
-        self.stats=row.to_dict()
-        self.stats['team_position']='Reserves'
+    facestats = ['short_name', 'nationality', 'overall', 'player_positions',
+                 'team_position', 'pace', 'shooting', 'passing', 'dribbling',
+                 'defending', 'physic']
+
+    def __init__(self, row):
+        self.stats = row.to_dict()
+        self.stats['team_position'] = 'Reserves'
         self.setPosition()
 
     def showPlayer(self):
-        temp=pd.DataFrame(self.stats,index=[0])
-        temp=temp.T
-        temp=temp.dropna()
+        temp = pd.DataFrame(self.stats, index=[0])
+        temp = temp.T
+        temp = temp.dropna()
         print(tabulate(temp))
         pass
 
     def setPosition(self):
         if 'B' in self.stats['player_positions'].split(",")[0]:
-            self.stats['player_positions']='Defender'
+            self.stats['player_positions'] = 'Defender'
         if 'M' in self.stats['player_positions'].split(",")[0]:
-            self.stats['player_positions']='Midfielder'
-        for x in ['S','F','W']:
+            self.stats['player_positions'] = 'Midfielder'
+        for x in ['S', 'F', 'W']:
             if x in self.stats['player_positions'].split(",")[0]:
-                self.stats['player_positions']='Attacker'
-        
+                self.stats['player_positions'] = 'Attacker'
         pass
