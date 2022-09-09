ecb95d4e581c3f77b36b79849cbf214a9b2ae523
Megh <meghkhaire@gmail.com>
2022-09-10 00:14:24 +0530

    Move odds to constants module

--- simulator/leagues.py
+++ simulator/leagues.py
@@ -1,62 +1,61 @@
-from teams import *
-import random
-import time
-import match as m
-import pandas as pd
-from odds import *
-from fuzzywuzzy import fuzz
-from itertools import permutations
 import copy
-import pprint
+from fuzzywuzzy import fuzz
+import pandas as pd
+import tabulate
+import simulator.match as m
+from simulator.teams import Team
+from simulator.constants.odds import sco
+
+
 class League:
-    country={1:'spain',2:'england',3:'germany',4:'italy',5:'france'}
-    def __init__(self,option):
-        self.players={}
-        self.teams={}
-        self.leaguename=league_options[League.country[option]]['name']
-        self.names=league_options[League.country[option]]['teams']  #self.names=names of the teams . will be changed soon 13/06/2020
+    country = {1: 'spain', 2: 'england', 3: 'germany', 4: 'italy', 5: 'france'}
+
+    def __init__(self, option):
+        self.players = {}
+        self.teams = {}
+        self.leaguename = sco.league_options[League.country[option]]['name']
+        # self.names=names of the teams . will be changed soon 13/06/2020
+        self.names = sco.league_options[League.country[option]]['teams']
         self.setTeams()
         self.setPlayers()
-        self.schedule=self.create_balanced_round_robin(self.names)
-        self.week=0
-        self.standings=self.initTable()
-
+        self.schedule = self.create_balanced_round_robin(self.names)
+        self.week = 0
+        self.standings = self.initTable()
 
     def initTable(self):
-        table=pd.DataFrame(columns=['Club','Matches Played','Wins','Draws','Losses','Points','GF','GA','GD'])
+        table = pd.DataFrame(columns=[
+                             'Club', 'Matches Played', 'Wins', 'Draws', 'Losses', 'Points', 'GF', 'GA', 'GD'])
         for team in self.names:
-            row=pd.DataFrame([[team,0,0,0,0,0,0,0,0]],columns=['Club','Matches Played','Wins','Draws','Losses','Points','GF','GA','GD'])
-            table=table.append(row)
-        table=table.reset_index(drop=True)
-        table.index=table.index+1
+            row = pd.DataFrame([[team, 0, 0, 0, 0, 0, 0, 0, 0]], columns=[
+                               'Club', 'Matches Played', 'Wins', 'Draws', 'Losses', 'Points', 'GF', 'GA', 'GD'])
+            table = table.append(row)
+        table = table.reset_index(drop=True)
+        table.index = table.index + 1
         return table
 
-
     def showStandings(self):
-        print(tabulate(self.standings,headers=self.standings.columns,tablefmt='github'))
-
+        print(tabulate(self.standings, headers=self.standings.columns, tablefmt='github'))
 
     def setTeams(self):
         for team in self.names:
-            t=Team(team)
-            self.teams[team]=t
-    
+            t = Team(team)
+            self.teams[team] = t
+
     def searchTeam(self):
-        query=input("\nEnter the name of the team you want to search\n")
+        query = input("\nEnter the name of the team you want to search\n")
         for team in self.teams.keys():
-            Partial_Ratio = fuzz.partial_ratio(query.lower(),team.lower())
-            if Partial_Ratio>90:
+            Partial_Ratio = fuzz.partial_ratio(query.lower(), team.lower())
+            if Partial_Ratio > 90:
                 self.teams[team].showTeam()
 
     def searchPlayer(self):
-        query=input("\nEnter the name of the player you want to search\n")
+        query = input("\nEnter the name of the player you want to search\n")
         for player in self.players.keys():
-            Partial_Ratio = fuzz.partial_ratio(query.lower(),player.lower())
-            if Partial_Ratio>90:
+            Partial_Ratio = fuzz.partial_ratio(query.lower(), player.lower())
+            if Partial_Ratio > 90:
                 self.players[player].showPlayer()
 
     def showPlayers(self):
-        df=pd.DataFrame()
         for team in self.teams.values():
             team.showPlayers(team.players)
 
@@ -69,54 +68,55 @@ class League:
             team.showTeam()
     pass
 
-    def simMatch(self,home_team_name,away_team_name):
-        home_team=self.teams[home_team_name]
-        away_team=self.teams[away_team_name]
-        match=m.Match(home_team,away_team)
+    def simMatch(self, home_team_name, away_team_name):
+        home_team = self.teams[home_team_name]
+        away_team = self.teams[away_team_name]
+        match = m.Match(home_team, away_team)
         match.showResult()
         self.updateTable(match)
         pass
 
-    def updateTable(self,match):
-        (result,winner,loser)=match.Result()
-        table=self.standings
-        winnergoals=match.stats[winner]['Goal']
-        losergoals=match.stats[loser]['Goal']
-        dif=winnergoals-losergoals
-        if result=='Draw':
-            for team in [winner,loser]:
-                table.loc[(table['Club']==team.team_name)]+=['',1,0,1,0,1,winnergoals,losergoals,0]
+    def updateTable(self, match):
+        (result, winner, loser) = match.Result()
+        table = self.standings
+        winnergoals = match.stats[winner]['Goal']
+        losergoals = match.stats[loser]['Goal']
+        dif = winnergoals - losergoals
+        if result == 'Draw':
+            for team in [winner, loser]:
+                table.loc[(table['Club'] == team.team_name)] += ['', 1, 0, 1, 0, 1, winnergoals, losergoals, 0]
         else:
-            table.loc[(table['Club']==winner.team_name)]+=['',1,1,0,0,3,winnergoals,losergoals,dif]
-            table.loc[(table['Club']==loser.team_name)]+=['',1,0,0,1,0,losergoals,winnergoals,-dif]
+            table.loc[(table['Club'] == winner.team_name)] += ['', 1, 1, 0, 0, 3, winnergoals, losergoals, dif]
+            table.loc[(table['Club'] == loser.team_name)] += ['', 1, 0, 0, 1, 0, losergoals, winnergoals, -dif]
         pass
-        table=table.sort_values(by=['Points','GD','GF'],ascending=False)
-        table=table.reset_index(drop=True)
-        table.index=table.index+1
-        self.standings=table
+        table = table.sort_values(by=['Points', 'GD', 'GF'], ascending=False)
+        table = table.reset_index(drop=True)
+        table.index = table.index + 1
+        self.standings = table
 
     def simLeague(self):
-        while self.week<len(self.schedule):
+        while self.week < len(self.schedule):
             self.simWeek()
             self.showStandings()
         pass
 
-    
     def simWeek(self):
-        for h,a in self.schedule[self.week]:
-            self.simMatch(h,a)
-        self.week+=1
+        for h, a in self.schedule[self.week]:
+            self.simMatch(h, a)
+        self.week += 1
         pass
-    def create_balanced_round_robin(self,teams):
+
+    def create_balanced_round_robin(self, teams):
         """ Create a schedule for the players in the list and return it"""
         s = []
-        if len(teams) % 2 == 1: teams = teams + [None]
+        if len(teams) % 2 == 1:
+            teams = teams + [None]
         # manipulate map (array of indexes for list) instead of list itself
         # this takes advantage of even/odd indexes to determine home vs. away
         n = len(teams)
         map = list(range(n))
         mid = n // 2
-        for i in range(n-1):
+        for i in range(n - 1):
             l1 = map[:mid]
             l2 = map[mid:]
             l2.reverse()
@@ -133,11 +133,10 @@ class League:
             s.append(round)
             # rotate list by n/2, leaving last element at the end
             map = map[mid:-1] + map[:mid] + map[-1:]
-        temp=copy.copy(s)
+        temp = copy.copy(s)
         for item in temp:
-            l=[]
+            l = []
             for x in item:
                 l.append(x[::-1])
             s.append(l)
         return s
-
--- simulator/match.py
+++ simulator/match.py
@@ -1,128 +1,117 @@
-from odds import odds as odds
-from odds import shot_outcome
-import pprint as pp
-import random
 import copy
-import numpy as np
-import time
+import random
 from tabulate import tabulate
-from events import *
+from simulator.constants.odds import odds
+from events import Event
+
 
 class Match:
-    odds=odds
-    reverse={'Home':'Away','Away':'Home'}
-    eventkeys=list(odds[0]['Home']['Events'].keys())
-    foulkeys=['Free kick won','Yellow card','Second yellow card','Red card']
+    odds = odds
+    reverse = {'Home': 'Away', 'Away': 'Home'}
+    eventkeys = list(odds[0]['Home']['Events'].keys())
+    foulkeys = ['Free kick won', 'Yellow card',
+                'Second yellow card', 'Red card']
 
-    def __init__(self,home_side,away_side):
-        self.odds=copy.deepcopy(odds)
-        tlist=copy.deepcopy(Match.eventkeys)
-        tlist.extend(['On target','Saved','Off target','Blocked','Hit the bar','Goal'])
-        tside=dict(zip(tlist,[0]*len(tlist)))
-        self.home_stats=dict(zip(tlist,[0]*len(tlist)))
-        self.away_stats=dict(zip(tlist,[0]*len(tlist)))
-        self.home_side=home_side
-        self.away_side=away_side
-        self.sides={home_side:'Home',away_side:'Away'}
-        self.reverse={home_side:away_side,away_side:home_side}
+    def __init__(self, home_side, away_side):
+        self.odds = copy.deepcopy(odds)
+        tlist = copy.deepcopy(Match.eventkeys)
+        tlist.extend(['On target', 'Saved', 'Off target',
+                     'Blocked', 'Hit the bar', 'Goal'])
+        self.home_stats = dict(zip(tlist, [0] * len(tlist)))
+        self.away_stats = dict(zip(tlist, [0] * len(tlist)))
+        self.home_side = home_side
+        self.away_side = away_side
+        self.sides = {home_side: 'Home', away_side: 'Away'}
+        self.reverse = {home_side: away_side, away_side: home_side}
         self.setOdds()
-        self.matchevents=[]
-        self.stats=dict(zip([self.home_side,self.away_side],[copy.copy(self.home_stats),copy.copy(self.away_stats)]))      
-        self.home_players=home_side.players
-        self.home_squad=home_side.squad
-        self.away_players=away_side.players
-        self.away_squad=away_side.squad
-        self.events(home_side,away_side)
-        
-    
+        self.matchevents = []
+        self.stats = dict(zip([self.home_side, self.away_side], [
+                          copy.copy(self.home_stats), copy.copy(self.away_stats)]))
+        self.home_players = home_side.players
+        self.home_squad = home_side.squad
+        self.away_players = away_side.players
+        self.away_squad = away_side.squad
+        self.events(home_side, away_side)
+
     def setOdds(self):
-      haf=self.home_side.attack/self.away_side.defence
-      aaf=self.away_side.attack/self.home_side.defence
-      hdf=(self.home_side.defence**2*self.home_side.midfield)/(self.away_side.attack**2*self.away_side.midfield)
-      adf=(self.away_side.defence**2*self.away_side.midfield)/(self.home_side.attack**2*self.home_side.midfield)
-      for minute in range(100):
-        self.odds[minute]['Home']['Events']['Attempt']=self.odds[minute]['Home']['Events']['Attempt']/(adf**2.33)
-        self.odds[minute]['Away']['Events']['Attempt']=self.odds[minute]['Away']['Events']['Attempt']/(hdf**2.33)
-     
-      pass
+        hdf = (self.home_side.defence**2 * self.home_side.midfield) / (self.away_side.attack**2 * self.away_side.midfield)
+        adf = (self.away_side.defence**2 * self.away_side.midfield) / (self.home_side.attack**2 * self.home_side.midfield)
+        for minute in range(100):
+            self.odds[minute]['Home']['Events']['Attempt'] = self.odds[minute]['Home']['Events']['Attempt'] / (adf**2.33)
+            self.odds[minute]['Away']['Events']['Attempt'] = self.odds[minute]['Away']['Events']['Attempt'] / (hdf**2.33)
+        pass
 
-    
-    def events(self,home_side,away_side):
-      for minute in range(100):
-        for _ in range(135):                  
-          if(random.uniform(0,1)<self.odds[minute]['Event']):
-            plist=[]
-            plist.append(self.odds[minute]['Home']['Probability'])
-            plist.append(self.odds[minute]['Away']['Probability'])
-            side = random.choices([self.home_side,self.away_side],plist)[0]
-            event=random.choices(Match.eventkeys,list(self.odds[minute][self.sides[side]]['Events'].values()))[0]
-            if event not in Match.foulkeys:
-              e=Event(event,side,minute)
-              e.setSides(home_side,away_side)
-              self.addEvent(e)
-        # if minute == 45:
-          # self.showStats()
-          # input('Press any key to start next half')
+    def events(self, home_side, away_side):
+        for minute in range(100):
+            for _ in range(135):
+                if(random.uniform(0, 1) < self.odds[minute]['Event']):
+                    plist = []
+                    plist.append(self.odds[minute]['Home']['Probability'])
+                    plist.append(self.odds[minute]['Away']['Probability'])
+                    side = random.choices(
+                        [self.home_side, self.away_side], plist)[0]
+                    event = random.choices(Match.eventkeys, list(
+                        self.odds[minute][self.sides[side]]['Events'].values()))[0]
+                    if event not in Match.foulkeys:
+                        e = Event(event, side, minute)
+                        e.setSides(home_side, away_side)
+                        self.addEvent(e)
 
+    def addEvent(self, event):
+        for e in event.checkEvent():
+            if e.event == 'Substitution':
+                if self.stats[e.side][e.event] < 3:
+                    self.trackEvents(e)
+            else:
+                self.trackEvents(e)
+            self.showEvent(e)
+            self.matchevents.append(e)
 
-    def addEvent(self,event):
-      for e in event.checkEvent():
-        if e.event=='Substitution':
-          if self.stats[e.side][e.event]<3:
-            self.trackEvents(e)
+    def trackEvents(self, event):
+        if event.side == self.home_side:
+            self.home_stats[event.event] = self.home_stats[event.event] + 1
         else:
-          self.trackEvents(e)
-        self.showEvent(e)
-        # time.sleep(0.01)
-        self.matchevents.append(e)
-    
-    def trackEvents(self,event):
-      if event.side == self.home_side:
-        self.home_stats[event.event]=self.home_stats[event.event]+1
-      else:
-        self.away_stats[event.event]=self.away_stats[event.event]+1
-      self.stats=dict(zip([self.home_side,self.away_side],[copy.copy(self.home_stats),copy.copy(self.away_stats)]))
-
+            self.away_stats[event.event] = self.away_stats[event.event] + 1
+        self.stats = dict(zip([self.home_side, self.away_side], [
+                          copy.copy(self.home_stats), copy.copy(self.away_stats)]))
 
     def showResult(self):
-      self.home_goals=self.stats[self.home_side]['Goal']
-      self.away_goals=self.stats[self.away_side]['Goal']
-      if self.home_goals>self.away_goals:
-        print(f"{self.home_side.team_name} won the match")
-        print(f"Score {self.home_goals} - {self.away_goals}")
-      elif self.away_goals>self.home_goals:
-        print(f"{self.away_side.team_name} won the match")
-        print(f"Score {self.home_goals} - {self.away_goals}")
-      else:
-        print(f"The match between {self.home_side.team_name} and {self.away_side.team_name} was a Draw")
-        print(f"Score {self.home_goals} - {self.away_goals}")
+        self.home_goals = self.stats[self.home_side]['Goal']
+        self.away_goals = self.stats[self.away_side]['Goal']
+        if self.home_goals > self.away_goals:
+            print(f"{self.home_side.team_name} won the match")
+            print(f"Score {self.home_goals} - {self.away_goals}")
+        elif self.away_goals > self.home_goals:
+            print(f"{self.away_side.team_name} won the match")
+            print(f"Score {self.home_goals} - {self.away_goals}")
+        else:
+            print(
+                f"The match between {self.home_side.team_name} and {self.away_side.team_name} was a Draw")
+            print(f"Score {self.home_goals} - {self.away_goals}")
 
     def Result(self):
-      hg=self.stats[self.home_side]['Goal']
-      ag=self.stats[self.away_side]['Goal']
-      if hg==ag:
-        return('Draw',self.home_side,self.away_side)
-      elif hg>ag:
-        return('Win',self.home_side,self.away_side)
-      else:
-        return('Win',self.away_side,self.home_side)
+        hg = self.stats[self.home_side]['Goal']
+        ag = self.stats[self.away_side]['Goal']
+        if hg == ag:
+            return('Draw', self.home_side, self.away_side)
+        elif hg > ag:
+            return('Win', self.home_side, self.away_side)
+        else:
+            return('Win', self.away_side, self.home_side)
 
-      pass
     def getGoals(self):
-      return self.home_goals,self.away_goals
+        return self.home_goals, self.away_goals
 
     def showStats(self):
-      table=[]
-      for stat in list(self.stats[self.home_side].keys()):
-        l=[]
-        l.append(stat)
-        for team in [self.home_side,self.away_side]:
-          l.extend([self.stats[team][stat]])
-        table.append(l)
-      print(tabulate(table,headers=['' ,self.home_side.team_name,self.away_side.team_name]))
+        table = []
+        for stat in list(self.stats[self.home_side].keys()):
+            l = []
+            l.append(stat)
+            for team in [self.home_side, self.away_side]:
+                l.extend([self.stats[team][stat]])
+            table.append(l)
+        print(tabulate(table, headers=[
+              '', self.home_side.team_name, self.away_side.team_name]))
 
-
-
-    def showEvent(self,e):
-      print(str(e.minute)+"'", e.side.team_name,e.event,e.player.stats['short_name'])
-      
+    def showEvent(self, e):
+        print(str(e.minute) + "'", e.side.team_name, e.event, e.player.stats['short_name'])
