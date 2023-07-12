1767609bbfb4803705815a6087374a88f57a34c3
Megh <meghkhaire@gmail.com>
2023-07-12 09:55:16 +0530

    Simplify implementation of Manager class
    - Remove unused fields.
    - Remove unwanted functions.
    - Randomly select a manager and formation for each team.

--- simulator/team.py
+++ simulator/team.py
@@ -1,41 +1,25 @@
 import numpy as np
 import pandas as pd
-from tabulate import tabulate
 
-import simulator.player as p
-from manager import *
+from simulator.player import Player
+from manager import Manager
 
 df_player_data = pd.read_pickle("simulator/resources/player_data")
 
 class Team:
-    attlist = [
-        "long_name",
-        "nationality",
-        "overall",
-        "player_positions",
-        "team_position",
-        "pace",
-        "shooting",
-        "passing",
-        "dribbling",
-        "defending",
-        "physical",
-    ]
-
     def __init__(self, team_name):
         self.players = {}
         self.squad = {}
         self.subs = {}
         self.reserves = {}
         self.team_name = team_name
-        self.addPlayers()
         self.attackers = {}
         self.midfielders = {}
         self.defenders = {}
         self.goalkeepers = {}
+        self.manager = Manager()
+        self.addPlayers()
         self.populatePositions()
-        self.manager = None
-        self.setManager()
         self.playingSquad()
         self.attack = round(
             np.array(
@@ -83,28 +67,7 @@ class Team:
     def addPlayers(self):
         tdf = df_player_data[df_player_data["club"] == self.team_name]
         for index, row in tdf.iterrows():
-            self.players[row["long_name"]] = p.Player(row)
-
-    def setManager(self):
-        self.manager = Manager()
-
-    def showPlayers(self, player_dict=None):
-        if player_dict == None:
-            player_dict = self.players
-        df = pd.DataFrame()
-        i = 1
-        for player in player_dict.values():
-            temp = pd.DataFrame(player.stats, index=[i], columns=p.Player.facestats)
-            df = df.append(temp)
-            i += 1
-        print(
-            tabulate(
-                df,
-                headers=[c.capitalize() for c in df.columns],
-                tablefmt="psql",
-                stralign="left",
-            )
-        )
+            self.players[row["long_name"]] = Player(row)
 
     def populatePositions(self):
         for player in self.players.values():
@@ -117,35 +80,8 @@ class Team:
             if player.stats["player_positions"] == "GK":
                 self.goalkeepers[player.stats["long_name"]] = player
 
-    def showManager(self):
-        table_data = []
-        table_data.append(
-            [
-                self.manager.stats["long_name"],
-                "Manager",
-                self.manager.stats,
-                self.manager.ivs,
-                self.manager.formation,
-            ]
-        )
-        print(tabulate(table_data))
-
-    def showTeam(
-        self,
-        player_dict=None,
-        position=["striker", "midfielder", "defender", "goalkeeper"],
-    ):
-        self.Substitutes()
-        self.showPlayers()
-        self.showStats()
-        print(self.squad)
-
-    def showStats(self):
-        print(
-            f"{self.team_name}'s Stats\nAttack = {self.attack}\nMidfield = {self.midfield}\nDefence = {self.defence}"
-        )
-
     def playingSquad(self):
+        [num_attackers, num_midfielders, num_defenders] = self.manager.formation
         squad = {}
         squad_attackers = {}
         squad_midfielders = {}
@@ -153,19 +89,19 @@ class Team:
         squad_gk = {}
         attackers = list(self.attackers.values())
         attackers.sort(key=lambda x: x.stats["overall"], reverse=True)
-        for player in attackers[0 : self.manager.strikers]:
+        for player in attackers[0 : num_attackers]:
             player.stats["team_position"] = "Starter"
             squad_attackers[player.stats["long_name"]] = player
 
         midfielders = list(self.midfielders.values())
         midfielders.sort(key=lambda x: x.stats["overall"], reverse=True)
-        for player in midfielders[0 : self.manager.midfielders]:
+        for player in midfielders[0 : num_midfielders]:
             player.stats["team_position"] = "Starter"
             squad_midfielders[player.stats["long_name"]] = player
 
         defenders = list(self.defenders.values())
         defenders.sort(key=lambda x: x.stats["overall"], reverse=True)
-        for player in defenders[0 : self.manager.defenders]:
+        for player in defenders[0 : num_defenders]:
             player.stats["team_position"] = "Starter"
             squad_defenders[player.stats["long_name"]] = player
 
@@ -182,16 +118,3 @@ class Team:
             }
         )
         self.squad = squad
-
-    def Substitutes(self):
-        subs = [x for x in self.players.keys() if x not in self.squad.keys()]
-        subs.sort(key=lambda x: self.players[x].stats["overall"], reverse=True)
-        for player in subs[:7]:
-            self.players[player].stats["team_position"] = "Substitute"
-            self.subs[player] = self.players[player]
-        reserves = [
-            r
-            for r in self.players.items()
-            if (r not in self.squad.items() and r not in self.subs.items())
-        ]
-        self.reserves.update(reserves)
